4ccf6fea312a5b69ac6bf2b27e04021c3e43b8ee
Blerand <blerand.buzhalaakju.at>
2025-12-05 09:31:18 +0100

    song

--- 05_song.py
+++ 05_song.py
@@ -35,21 +35,49 @@ Beispiel Ergebnis:
 """
 
 # TODO: Erstelle hier die Klasse Song
+class Song:
+    def __init__(self, titel, interpreten):
+        self.titel = titel
+        self.interpreten = interpreten
+        print(f"Neuer Song mit Titel {self.titel} & Interpreten {self.interpreten} wurde erstellt!")
+    
+    def zeige_info(self):
+        print(f"Titel {self.titel}, Interpreten {self.interpreten} wurde erstellt!")  
 
+    def interpret_hinzufuegen(self, neuer_interpret):
+        self.interpreten.append(neuer_interpret)
+        print(f"🎤 {neuer_interpret} wurde hinzugefügt")
+        print(f"Alle Interpreten: {self.interpreten}")
+    
+    def anzahl_interpreten(self):
+        return len(self.interpreten)
+    
+    def play(self):
+        
+        print(f"▶️ Song '{self.titel}' wird gespielt...")
 
 # TODO: Erstelle einen Song mit einem Titel und 2 Interpreten deiner Wahl
-
+song_1 = Song(titel="Die with a smile", interpreten=["Bruno Mars", "Lady Gaga"])
+song_2 = Song(titel="Zombie", interpreten=["Cranberries"])
+rock_song = Song(titel="My Only Angel", interpreten=["Yungblud", "Aerosmith"])
 
 # TODO: Zeige die Song-Info
+song_1.zeige_info()
 
 
 # TODO: Füge einen weiteren Interpreten hinzu
+song_1.interpret_hinzufuegen("DJ Lenzi")
+song_1.interpret_hinzufuegen("DJ Obdocha")
 
 
 # TODO: Zeige die Anzahl der Interpreten
+nr_artists = song_1.anzahl_interpreten()
+print(nr_artists)
+print(song_1.anzahl_interpreten()) #Das gleiche wie oben, nur wird das Ergebnis nicht zwischengespeichert
 
 
 # TODO: Zeige die Song-Info erneut
-
+song_1.zeige_info()
 
 # TODO: Spiele den Song ab
+song_1.play()
